1a4a8efa207d9618d1832848201a5ac5b03472d3
PerizatD <84829957+PerizatD@users.noreply.github.com>
2023-02-06 15:21:59 -0400

    Update FirstGitHubClass.java
    Hey changed to Hello

--- src/day1github/FirstGitHubClass.java
+++ src/day1github/FirstGitHubClass.java
@@ -2,7 +2,7 @@ package day1github;
 
 public class FirstGitHubClass {
     public static void main(String[] args) {
-        System.out.println("Hey GitHub!");
+        System.out.println("Hello GitHub!");
         System.out.println(1+1);
     }
 }
